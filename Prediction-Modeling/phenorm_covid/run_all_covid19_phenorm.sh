363cea0565713715b54a649aebf92f8fae1cad13
Brian Williamson <brian.d.williamson@kp.org>
2022-12-29 17:00:58 -0800

    Add preprocessing

--- Prediction-Modeling/phenorm_covid/run_all_covid19_phenorm.sh
+++ Prediction-Modeling/phenorm_covid/run_all_covid19_phenorm.sh
@@ -3,11 +3,18 @@
 # install required packages (only need to run this one time)
 source phenorm_covid/00_install_packages.sh 
 
+# preprocess datasets (uses analysis-specific code); wouldn't need this if input
+# data met the requirements for downstream code, namely, that:
+# 1. input data for an analysis consist of only the relevant variables for that analysis (studyid, outcome, train/eval set designation, utilization (if any), weight (if any), silver labels, structured data features, NLP features)
+# 2. all data are of the correct format (e.g., for binary variables, 0/1 [not characters])
+
+source phenorm_covid/00_preprocess_covid19_datasets.sh
+
 # process datasets
-source phenorm_covid/01_process_covid19_datasets.sh
+source phenorm_covid/01_process_datasets.sh
 
 # run internal model training and evaluation
-source phenorm_covid/02_run_covid19_phenorm_internal.sh
+source phenorm_covid/02_run_phenorm_internal.sh
 
 # run external model evaluation
-source phenorm_covid/03_run_covid19_phenorm_external.sh
+source phenorm_covid/03_run_phenorm_external.sh
