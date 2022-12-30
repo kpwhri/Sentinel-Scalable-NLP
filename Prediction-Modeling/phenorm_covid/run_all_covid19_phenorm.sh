8c977a73aad33f8ea4c1ff6af7f5365902f23f08
Brian Williamson <brian.d.williamson@kp.org>
2022-12-30 10:00:21 -0800

    Add line for when 00_preprocess should be run

--- Prediction-Modeling/phenorm_covid/run_all_covid19_phenorm.sh
+++ Prediction-Modeling/phenorm_covid/run_all_covid19_phenorm.sh
@@ -7,6 +7,7 @@ source phenorm_covid/00_install_packages.sh
 # data met the requirements for downstream code, namely, that:
 # 1. input data for an analysis consist of only the relevant variables for that analysis (studyid, outcome, train/eval set designation, utilization (if any), weight (if any), silver labels, structured data features, NLP features)
 # 2. all data are of the correct format (e.g., for binary variables, 0/1 [not characters])
+# only have to run this code one time per combination of (outcome, silver labels, all possible structured data features, NLP features)
 source phenorm_covid/00_preprocess_covid19_datasets.sh
 
 # process datasets
