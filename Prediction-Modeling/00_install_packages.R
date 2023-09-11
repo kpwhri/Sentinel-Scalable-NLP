11d486ba0ae9c84b53ab6e9bc4e9703cedf1c5f9
Brian Williamson <brian.d.williamson@kp.org>
2023-09-11 11:21:42 -0700

    Set require to use character, don't use lock file

--- Prediction-Modeling/00_install_packages.R
+++ Prediction-Modeling/00_install_packages.R
@@ -5,8 +5,9 @@
 package_list <- c("optparse", "here", "PheNorm", "ROCR", "WeightedROC", "tidyverse", "cowplot")
 # check to make sure each package is installed; if not, install it
 lapply(as.list(package_list), function(package) {
-  is_package_avail <- require(package)
+  is_package_avail <- require(package, character.only = TRUE)
   if (!is_package_avail) {
-    install.packages(package, repos = "https://cran.rstudio.com")
+    install.packages(package, repos = "https://cran.rstudio.com", INSTALL_opts = "--no-lock")
   }
 })
+
