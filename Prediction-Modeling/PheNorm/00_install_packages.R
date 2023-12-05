edb6e1c7ab5155b93eb4bd2a79a87c059183b215
Brian Williamson <brian.d.williamson@kp.org>
2023-12-04 16:57:50 -0800

    Install correct package list

--- Prediction-Modeling/PheNorm/00_install_packages.R
+++ Prediction-Modeling/PheNorm/00_install_packages.R
@@ -5,7 +5,7 @@
 package_list <- c("optparse", "here", "PheNorm", "ROCR", "WeightedROC", "cvAUC")
 tidy_package_list <- c("ggplot2", "dplyr", "readr", "stringr", "cowplot")
 # check to make sure each package is installed; if not, install it
-lapply(as.list(package_list), function(package) {
+lapply(as.list(c(package_list, tidy_package_list)), function(package) {
   is_package_avail <- require(package, character.only = TRUE)
   if (!is_package_avail) {
     install.packages(package, repos = "https://cran.rstudio.com", INSTALL_opts = "--no-lock")
